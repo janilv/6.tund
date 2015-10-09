b4cff6ae41267cfe295245b7372bd2b2d490f9db
Jan-Erik Ilves <janilv@greeny.cs.tlu.ee>
2015-10-09 13:28:06 +0300

    Delete function added

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
 		
 		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
 		
-		$stmt = $mysqli->prepare("SELECT id, user_id, number_plate, color FROM car_plates");
+		$stmt = $mysqli->prepare("SELECT id, user_id, number_plate, color FROM car_plates WHERE deleted IS NULL");
 		$stmt->bind_result($id, $user_id, $number_plate, $color_from_db);
 		$stmt->execute();
 		
@@ -24,6 +24,8 @@
 			$car = new StdClass();
 			$car->id = $id;
 			$car->number_plate = $number_plate;
+			$car->user_id = $user_id;
+			$car->color = $color_from_db;
 			
 			// lisame selle massiivi
 			array_push($array, $car);
@@ -42,8 +44,27 @@
 	}
 	
 	
+	function deleteCar($id_to_be_deleted){
+		
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		
+		$stmt = $mysqli->prepare("UPDATE car_plates SET deleted=NOW() WHERE id=?");
+		$stmt->bind_param("i", $id_to_be_deleted);
+		
+		if($stmt->execute()){
+			// sai edukalt kustutatud
+			header("Location: table.php");
+			
+		}
+		
+		$stmt->close();
+		$mysqli->close();
+		
+	}
 	
-	//$name = "Jan";
+	
+	
+	//$name = "Romil";
 	//echo "Tere ".$name;
 	
 	//var_dump("asdasdasdasd");
--- table.php
+++ table.php
@@ -3,29 +3,46 @@
 	// table.php
 	require_once("functions.php");
 	
+	//kas kasutaja tahab kustutada
+	// kas aadressireal on ?delete=??!??!?!
+	if(isset($_GET["delete"])){
+		
+		// saadan kaasa id, mida kustutada
+		deleteCar($_GET["delete"]);
+		
+	}
+	
+	
+	
 	$car_list = getCarData();
 	//var_dump($car_list);
-	
 ?>
 <table border=1 >
 	<tr>
 		<th>id</th>
+		<th>kasut id</th>
 		<th>auto nr märk</th>
+		<th>värv</th>
+		<th>X</th>
 	</tr>
-
+	
 	<?php
 	
-		// iga massiivis oleva elemendi kohta
+		// iga massiivis olema elemendi kohta
 		// count($car_list) - massiivi pikkus
 		for($i = 0; $i < count($car_list); $i++){
-			// $i = $i +1; sama, mis $i +=1; sama, mis $i++;
+			// $i = $i +1; sama mis $i += 1; sama mis $i++;
 			echo "<tr>";
 			
 			echo "<td>".$car_list[$i]->id."</td>";
+			echo "<td>".$car_list[$i]->user_id."</td>";
 			echo "<td>".$car_list[$i]->number_plate."</td>";
+			echo "<td>".$car_list[$i]->color."</td>";
+			echo "<td><a href='?delete=".$car_list[$i]->id."'>X</a></td>";
 			
 			echo "</tr>";
 		}
-		
+	
 	?>
+
 </table>
