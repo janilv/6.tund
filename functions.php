5a66cfa81e5b2de399bbb1403ab7a8b136b68c7f
Jan-Erik Ilves <janilv@greeny.cs.tlu.ee>
2015-10-09 12:52:13 +0300

    big update

--- functions.php
+++ functions.php
@@ -7,11 +7,45 @@
 	//loome uue funktsiooni, et küsida ab'ist andmeid
 	function getCarData(){
 		
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
 		
+		$stmt = $mysqli->prepare("SELECT id, user_id, number_plate, color FROM car_plates");
+		$stmt->bind_result($id, $user_id, $number_plate, $color_from_db);
+		$stmt->execute();
 		
+		// tühi massiiv kus hoiame objekte (1 rida andmeid)
+		$array = array();
+		
+		// tee tsüklit nii mitu korda, kui saad 
+		// ab'ist ühe rea andmeid
+		while($stmt->fetch()){
+			
+			// loon objekti iga while tsükli kord
+			$car = new StdClass();
+			$car->id = $id;
+			$car->number_plate = $number_plate;
+			
+			// lisame selle massiivi
+			array_push($array, $car);
+			//echo "<pre>";
+			//var_dump($array);
+			//echo "</pre>";
+			
+		}
+		
+		$stmt->close();
+		$mysqli->close();
+		
+		return $array;
 		
 		
 	}
 	
+	
+	
+	//$name = "Jan";
+	//echo "Tere ".$name;
+	
+	//var_dump("asdasdasdasd");
 	
 ?>
